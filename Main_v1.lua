bd3cde69419451918fb880ecb3ac8d3face8ac2f
ArskyCache <85579771+A1sky@users.noreply.github.com>
2021-06-17 14:10:42 -0400

    Create Main_v1.lua

---
+++ Main_v1.lua
@@ -0,0 +1,81 @@
+_G.Settings = {
+    ["Team ESP"] = true,
+    ["Enemy ESP"] = true,
+    ["Team Color"] = Color3.fromRGB(75, 93, 172),
+    ["Enemy Color"] = Color3.fromRGB(172, 77, 78)
+}
+
+local BodyParts = {
+    "Head",
+    "LowerTorso",
+    "UpperTorso"
+}
+
+local Targets = {}
+local Players = game:GetService("Players")
+local Holder = true
+
+while Holder == true and wait(1) do
+    if Holder == false then
+        break
+    end
+    
+    local TeamName = Players.LocalPlayer.Team
+
+    for _,Player in pairs(Players:GetPlayers()) do
+        local Error = pcall(function()
+            if _G.Settings["Team ESP"] == true then
+                if Player.Team == TeamName then
+                    if Player.Name == Players.LocalPlayer.Name then
+                        --Pass
+                    else
+                        table.insert(Targets, Player.Name)
+                    end
+                end
+            end
+            
+            if _G.Settings["Enemy ESP"] == true then
+                if Player.Team == TeamName then
+                    --Pass
+                else
+                    if Player.Name == Players.LocalPlayer.Name then
+                        --Pass
+                    else
+                        table.insert(Targets, Player.Name)
+                    end
+                end
+            end
+            
+            if _G.Settings["Enemy ESP"] and _G.Settings["Team ESP"] == false then
+                print("Venti [You disabled all esp]")
+            end
+        end)
+    end
+    
+    for _,Target in pairs(Targets) do
+        local Player = Players[Target]
+        local PChar = Player.Character
+        
+        for _,BodyPart in pairs(BodyParts) do
+            if PChar:FindFirstChild(BodyPart) then
+                if PChar[BodyPart]:FindFirstChild("discord.gg/Kenta") then
+                    PChar[BodyPart]:FindFirstChild("discord.gg/Kenta"):Destroy()
+                end
+                
+                local EspBOX = Instance.new("BoxHandleAdornment", PChar[BodyPart])
+                EspBOX.Adornee = PChar[BodyPart]
+                EspBOX.AlwaysOnTop = truesawdw
+                EspBOX.ZIndex = 2
+                EspBOX.Name = "discord.gg/Kenta"
+                
+                if Player.Team == TeamName then
+                    EspBOX.Color3 = _G.Settings["Team Color"]
+                else
+                    EspBOX.Color3 = _G.Settings["Enemy Color"]
+                end
+            else
+                print("Venti [Couldn't Find "..BodyPart.." in "..Player.Name.."]")
+            end
+        end
+    end
+end
